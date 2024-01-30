5aee586ee0e54aa35099657f6b9c0162f77d9f9e
chiloanerk <chiloanerk@gmail.com>
2024-01-30 17:06:46 +0200

    implemented sidemenu

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import SideMenu from "./SideMenu.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <SideMenu />
     <App />
   </React.StrictMode>,
 )
